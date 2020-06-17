617f850cd72b44ba3560a76057230a881770bffb
Scott <scott.lee.matton@gmail.com>
2020-06-17 19:43:30 -0400

    add runtime

--- cmd/nnservice.go
+++ cmd/nnservice.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"runtime"
 
 	"github.com/smatton/go-nnservice/pkg/network"
 	"github.com/smatton/go-nnservice/pkg/server"
@@ -11,12 +12,12 @@ import (
 
 var (
 	PORT string
+	CPU  int
 )
 
 func main() {
 
 	flag.StringVar(&PORT, "port", "9023", "port to start registry on")
-
 	flag.Parse()
 	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
 
@@ -25,6 +26,9 @@ func main() {
 		logger.Println("Couldn't determine hostname, starting on loopback 127.0.0.1")
 		myIP = "127.0.0.1"
 	}
+
+	numCPUs := runtime.NumCPU()
+	runtime.GOMAXPROCS(numCPUs)
 
 	myserver := server.New(myIP + ":" + PORT)
 
